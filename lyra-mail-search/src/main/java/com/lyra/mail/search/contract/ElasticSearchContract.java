6d94f5648f155058c69830bddd91debfea4fd047
RainbowSorcery <365373011.com>
2022-05-07 14:04:54 +0800

    搜索服务完成, 但是页面没有写.

--- lyra-mail-search/src/main/java/com/lyra/mail/search/contract/ElasticSearchContract.java
+++ lyra-mail-search/src/main/java/com/lyra/mail/search/contract/ElasticSearchContract.java
@@ -1,5 +1,6 @@
 package com.lyra.mail.search.contract;
 
 public class ElasticSearchContract {
-    public static final String PRODUCT_INDEX = "product";
+    public static final String PRODUCT_INDEX = "lyramalil_product";
+    public static final Integer PRODUCT_PAGE_SIZE = 10;
 }
